6543eff699fc1cd69261a46ba33fc9bceb26c5a8
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-31 09:27:39 +0100

    setup public folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const path = require("path");
 
 /**
  *  Simple Express server setup
+ *  @description This code sets up a basic Express server that serves static files
+ *               from a "public" directory and listens on a specified port.
  */
 const app = express();
 const port = process.env.PORT || 3000;
 
 /**
+ * Public folder
+ * @description  This serves static files from the "public" directory.
+ *               You can place your HTML, CSS, JavaScript, and images here.
+ */
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
  * Routes
+ *  @description This sets up a simple route that responds with "Hello, World!" when
+ *               the root URL is accessed.
  */
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -15,6 +27,8 @@ app.get("/", (req, res) => {
 
 /**
  * Start the server
+ *  @description This starts the server and listens on the specified port.
+ *               It logs a message to the console when the server is running.
  */
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
