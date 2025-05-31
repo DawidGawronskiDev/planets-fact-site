82331f621cadf4e534e203448fe88942a3ecfc43
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-31 10:08:10 +0100

    create Planet model

---
+++ src/models/Planet.js
@@ -0,0 +1,45 @@
+const db = require("../lib/db");
+
+/**
+ *  Planet Model
+ *   @description This model interacts with the planets table in the database.
+ *                It provides a method to retrieve all planets.
+ */
+class Planet {
+  /**
+   * Retrieves all planets from the database.
+   * @returns {Promise<Array>} A promise that resolves to an array of planet objects.
+   */
+  static getAllPlanets() {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM planets`;
+      db.all(query, [], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  /**
+   * Retrieves a planet by its ID.
+   * @param {number} id - The ID of the planet to retrieve.
+   * @returns {Promise<Object>} A promise that resolves to a planet object.
+   */
+  static getPlanetById(id) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM planets WHERE id = ?`;
+      db.get(query, [id], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+}
+
+module.exports = Planet;
