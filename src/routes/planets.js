dcb66983c87ae7844765e22dd47062feeefd3d08
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-31 10:08:35 +0100

    update planets routes

--- src/routes/planets.js
+++ src/routes/planets.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const planetController = require("../controllers/planets");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("planets", { title: "Planets" });
-});
+/**
+ *   Route to get all planets
+ *   @description This route retrieves all planets from the database and returns them as a JSON response.
+ *   @returns {Array} An array of planet objects.
+ *
+ */
+router.get("/", planetController.getAllPlanets);
+
+/**
+ *  Route to get a planet by its ID
+ *  @description This route retrieves a specific planet by its ID from the database and returns it as a JSON response.
+ *  @param {string} id - The ID of the planet to retrieve.
+ *  @returns {Object} A planet object with the specified ID.
+ *  @throws {404} If the planet with the specified ID is not found.
+ */
+router.get("/:id", planetController.getPlanetById);
 
 module.exports = router;
