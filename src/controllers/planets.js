fcc709fee28086082c3e18c0691b4eca07d33d5b
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-31 10:08:25 +0100

    create planets controller

---
+++ src/controllers/planets.js
@@ -0,0 +1,26 @@
+const Planet = require("../models/Planet");
+
+async function getAllPlanets(req, res) {
+  const planets = await Planet.getAllPlanets();
+
+  if (!planets) {
+    return res.status(404).json({ error: "No planets found" });
+  }
+
+  res.status(200).json(planets);
+}
+
+async function getPlanetById(req, res) {
+  const planet = await Planet.getPlanetById(req.params.id);
+
+  if (!planet) {
+    return res.status(404).json({ error: `Planet with ID ${id} not found` });
+  }
+
+  res.status(200).json(planet);
+}
+
+module.exports = {
+  getAllPlanets,
+  getPlanetById,
+};
